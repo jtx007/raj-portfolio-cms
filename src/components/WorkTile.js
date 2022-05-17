4351ee38cb31a651383ba3bd357966113c400848
jtx007 <jamesjacobthomas7@gmail.com>
2022-05-16 20:55:49 -0400

    worktile component and static about and contact pages

---
+++ src/components/WorkTile.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+const WorkTile = ({ image, title, type }) => {
+  return (
+    <div className="work-tile">
+      <div className="work-tile-overlay">
+        <Link to={`${title.split(" ").join("")}`}>
+          <GatsbyImage
+            imgClassName="work-main-image"
+            alt={title}
+            image={image.childImageSharp.gatsbyImageData}
+          />
+          <div className="work-details">
+            <h3 className="work-title">{title}</h3>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default WorkTile
---
+++ src/md/contact.md
@@ -0,0 +1,3 @@
+---
+email: jamesjacobthomas7@gmail.com
+---
